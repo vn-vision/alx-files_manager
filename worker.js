7f4023ff9def3de1d8c830cb4d0cd3ecc01a6ec1
kZilla <kzilla@vn-z.zilla>
2024-10-02 12:21:03 +0300

    File Manager, refreshed

--- worker.js
+++ worker.js
@@ -62,14 +62,14 @@ userQueue.process(async (job, done) => {
   }
   console.log(`Welcome ${user.email}!`);
   try {
-    const mailSubject = 'Welcome to ALX-Files_Manager by B3zaleel';
+    const mailSubject = 'Welcome to ALX-Files_Manager by vn-vision';
     const mailContent = [
       '<div>',
       '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
+      'Welcome to <a href="https://github.com/vn-vision/alx-files_manager">',
       'ALX-Files_Manager</a>, ',
       'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      '<a href="https://github.com/vn-vision">vn-vision</a>. ',
       'We hope it meets your needs.',
       '</div>',
     ].join('');
